7343be98ec3c92693919860a2d4f643174482b83
lanly_dev <lanly-dev@users.noreply.github.com>
2019-11-02 12:50:46 -0700

    Pug

--- api/index.js
+++ api/index.js
@@ -7,9 +7,6 @@ app.post('/helloworld',  (req, res, next) => {
   res.json({hello: 'world'})
 })
 
-// Import API Routes
-app.use(users)
-
 // Export the server middleware
 module.exports = {
   path: '/api',
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,16 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        TtP-Seattle10-dashboard
-      </h1>
-      <h2 class="subtitle">
-        My wicked Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  .container
+    div
+      logo
+        h1.title
+          | TtP-Seattle10-dashboard
+        h2.subtitle
+          | My wicked Nuxt.js project
+        .links
+          a.button--green(href='https://nuxtjs.org/' target='_blank')
+            | Documentation
+          a.button--grey(href='https://github.com/nuxt/nuxt.js' target='_blank')
+            | GitHub
 </template>
 
 <script>

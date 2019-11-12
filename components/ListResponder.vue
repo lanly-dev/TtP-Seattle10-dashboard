844420a66680a23a8c3706866c61ed6a1ac7209d
lanly_dev <lanly-dev@users.noreply.github.com>
2019-11-12 00:24:57 -0800

    Tags colors

--- components/ListResponder.vue
+++ components/ListResponder.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
   span
-    .row(v-for='item in items')
-      .col-2 color
-      .col-10 {{ item.message }}
+    .row(v-for='team in responders')
+      .col-3 {{team.id}}
+      .col-9: small {{ team.name }} ETA-{{team.eta}}
 </template>
 <script>
 export default {
   data() {
     return {
-      items: [
-        { color: 'green', gender: 'male' },
-        { color: 'yellow', gender: 'female' },
-        { color: 'red', gender: 'female' },
-        { color: 'black', gender: 'male' }
+      responders: [
+        { id: 1, name: 'team1', eta: 10 },
+        { id: 2, name: 'team2', eta: 20 },
+        { id: 3, name: 'team3', eta: 5 },
+        { id: 4, name: 'team4', eta: 0 }
       ]
     }
   },
--- components/ListTarget.vue
+++ components/ListTarget.vue
@@ -1,22 +1,28 @@
 <template lang="pug">
   span
-    .row(v-for='item in items')
-      .col-2 color
-      .col-10 {{ item.message }}
+    .row(v-for='target in targets')
+      .col-3(:style='tagColor(target.color)')
+        span {{target.id}}
+      .col-9 {{ target.gender }}
 </template>
 <script>
 export default {
   data() {
     return {
-      items: [
-        { color: 'green', gender: 'male' },
-        { color: 'yellow', gender: 'female' },
-        { color: 'red', gender: 'female' },
-        { color: 'black', gender: 'male' }
+      targets: [
+        { id: 1, color: 'green', gender: 'male' },
+        { id: 2, color: 'yellow', gender: 'female' },
+        { id: 3, color: 'red', gender: 'female' },
+        { id: 4, color: 'black', gender: 'male' }
       ]
     }
   },
   methods: {
+    tagColor(color) {
+      let textColor = 'white'
+      if (color === 'yellow') textColor = 'black'
+      return { backgroundColor: color, color: textColor }
+    },
     itemClick() {
       console.log('clicked')
     }

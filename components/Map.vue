3d7824ef3da77ae5679c45751e3c358041abec65
lanly_dev <lanly-dev@users.noreply.github.com>
2019-11-14 22:02:53 -0800

    Popup CSS and update

--- components/Map.vue
+++ components/Map.vue
@@ -4,9 +4,10 @@
     gmap-marker(:key='`b-${index}`' v-for='(m, index) in emsTeams' :position='m.position' :clickable='true' @click='openInfoWindowTemplate(m)')
     gmap-info-window(:options='{maxWidth: 300}' :position='infoWindow.position' :opened='infoWindow.open' @closeclick='infoWindow.open=false')
       div
-        p {{infoWindow.id}}
-        p {{infoWindow.gender}}
-        p {{infoWindow.name}}
+        .row
+          .col-2 {{infoWindow.id}}
+          .col-10(v-if='infoWindow.gender') {{infoWindow.gender}}
+          .col-10(v-if='infoWindow.name') {{infoWindow.name}}
 </template>
 <script>
 export default {
@@ -57,5 +58,31 @@ export default {
 #map {
   height: 35em;
   width: 100%;
+}
+
+/*style the box which holds the text of the information window*/
+.gm-style .gm-style-iw {
+  background-color: white !important;
+  top: 0 !important;
+  left: 0 !important;
+  width: 150px !important;
+  /* height: 100% !important; */
+  /* min-height: 120px !important; */
+  padding-top: 10px;
+  display: block !important;
+}
+
+.gm-style div div div div div div div div {
+  background-color: white !important;
+  margin: 0;
+  padding: 0;
+  top: 0;
+  color: black;
+  font-size: 16px;
+}
+
+/*remove litte arrow bottom */
+.gm-style .gm-style-iw-t::after {
+  content: none !important;
 }
 </style>
